a011a80128337470b51886d89c1a38b642bec99f
BruceHi <BruceHi@users.github.com>
2020-12-26 15:20:44 +0800

    scrapy and process data

---
+++ work/products/products/pipelines.py
@@ -0,0 +1,49 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import pymysql
+
+
+class ProductsPipeline:
+    # 打开连接，建表
+    def open_spider(self, spider):
+        self.conn = pymysql.connect(host='localhost', user='root', password='123456',
+                                   database='db2', charset='utf8mb4')
+        self.cur = self.conn.cursor()
+        sql = """
+        CREATE TABLE shorts(
+        id INT auto_increment PRIMARY KEY,
+		phone_name varchar(100),
+        short varchar(400),
+        storage_time TIMESTAMP
+        );
+        """
+        try:
+            self.cur.execute(sql)
+            print('建表成功。')
+            self.conn.commit()
+        except:
+            print('建表失败，可能已经存在表了。')
+            self.conn.rollback()
+
+    def process_item(self, item, spider):
+        phone_name = item['phone_name']
+        short = item['short']
+        sql = 'insert into shorts(phone_name, short, storage_time) values (%s, %s, Now())'
+        try:
+            self.cur.execute(sql, [phone_name, short])
+            self.conn.commit()
+        except:
+            print('插入失败')
+            self.conn.rollback()
+        return item
+
+    # 关闭游标，关闭连接
+    def close_spider(self, spider):
+        self.cur.close()
+        self.conn.close()
---
+++ work/products/products/spiders/phone.py
@@ -0,0 +1,48 @@
+import scrapy
+from scrapy.selector import Selector
+from products.items import ProductsItem
+from copy import deepcopy
+
+
+class PhoneSpider(scrapy.Spider):
+    name = 'phone'
+    allowed_domains = ['smzdm.com']
+    start_urls = ['https://www.smzdm.com/fenlei/zhinengshouji/h5c4s0f0t0p1/#feed-main/']
+
+    # 解析商品链接，存储商品名称
+    def parse(self, response, **kwargs):
+        item = ProductsItem()
+        contents = Selector(response).xpath('//*[@id="feed-main-list"]/li[@class="feed-row-wide"]')
+        for content in contents[:10]:
+
+            phone_name = content.xpath('div/div[2]/h5/a/text()').get()
+            item['phone_name'] = phone_name.split(': ')[-1]
+            # print(phone_name)
+
+            link = content.xpath('div/div[2]/h5/a/@href').get()
+            # 深拷贝，不然总是显示最后一个
+            yield scrapy.Request(url=link, meta={'item': deepcopy(item)}, callback=self.parse2)
+
+    # 解析评论链接
+    def parse2(self, response):
+        item = response.meta['item']
+        pages = 1
+        comments_block = Selector(response).xpath('//*[@id="commentTabBlockNew"]/ul')
+        # 判断是否有多页
+        if len(comments_block) == 2:
+            pages = int(comments_block[-1].xpath('li[last()-3]/a/text()').get())
+
+        for i in range(1, pages+1):
+            link = response.url + 'p' + str(i)
+            # 重复的请求不过滤
+            yield scrapy.Request(url=link, meta={'item': deepcopy(item)}, callback=self.parse3, dont_filter=True)
+
+    # 提取评论
+    def parse3(self, response):
+        print('hello')
+        item = response.meta['item']
+        comments = Selector(response).xpath('//*[@id="commentTabBlockNew"]/ul[1]/li[@class="comment_list"]')
+        for comment in comments:
+            short = comment.xpath('div[2]/div[@class="comment_conWrap"]/div[1]/p/span/text()').get().strip()
+            item['short'] = short
+            yield item
